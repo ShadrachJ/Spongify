73db53fe7ae0cd1dbb95734eac1631ffe25083ba
Shadrach Jakes <shadjakes@gmail.com>
2019-08-28 20:24:19 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -5,26 +5,25 @@ $(document).ready(function(){
  	 var id =  this.id;
      switch(id) {
   	case "puppyLink":
-        console.log('puppy');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#puppies').css('display','block');
 		closeNav();
     break;
   	case "parentLink":
     	console.log('parent');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#parents').css('display','block');
 		closeNav();
     break;
     case "contactLink":
     	console.log('contact');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#contact').css('display','block');
 		closeNav();
     break;
     case "aboutLink":
     	console.log('about');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#about').css('display','block');
 		closeNav();
     break;
