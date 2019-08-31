b0088e63faea00ddcfa2f925f72207a7c4300477
StrongBro <shadjakes@gmail.com>
2019-08-31 15:22:17 -0400

    Most of the content
    Added images for puppies and parents, added content for these sections, changed the contact us a bit

--- controller.js
+++ controller.js
@@ -5,29 +5,23 @@ $(document).ready(function(){
  	 var id =  this.id;
      switch(id) {
   	case "puppyLink":
-        console.log('puppy');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#puppies').css('display','block');
 		closeNav();
     break;
   	case "parentLink":
     	console.log('parent');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#parents').css('display','block');
 		closeNav();
     break;
     case "contactLink":
     	console.log('contact');
-        $('.main div').css('display','none');
+        $('.main > div').css('display','none');
         $('#contact').css('display','block');
 		closeNav();
     break;
-    case "aboutLink":
-    	console.log('about');
-        $('.main div').css('display','none');
-        $('#about').css('display','block');
-		closeNav();
-    break;
+    
   default:
     	console.log('not listed');
 }
